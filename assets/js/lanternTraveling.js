2986d73ea79176f1a47d96a413a67b5b43ef7fd0
mike911209 <mike911209@gmail.com>
2024-04-26 15:17:34 +0800

    blog: add lanternTraveling day1 modals

--- assets/js/lanternTraveling.js
+++ assets/js/lanternTraveling.js
@@ -1,6 +1,44 @@
 'use strict';
 
 
+// testimonials variables
+const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
+console.log(testimonialsItem.length);
+const modalContainer = document.querySelector("[data-modal-container]");
+const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
+
+// modal variable
+const modalImg = document.querySelector("[data-modal-img]");
+const modalTitle = document.querySelector("[data-modal-title]");
+const modalText = document.querySelector("[data-modal-text]");
+
+// modal toggle function
+const testimonialsModalFunc = function () {
+  modalContainer.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+
+// add click event to all modal items
+for (let i = 0; i < testimonialsItem.length; i++) {
+
+  testimonialsItem[i].addEventListener("click", function () {
+
+    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
+    console.log(modalImg.src);
+    // modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
+    // modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
+    // modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
+
+    testimonialsModalFunc();
+
+  });
+}
+
+// add click event to modal close button
+modalCloseBtn.addEventListener("click", testimonialsModalFunc);
+overlay.addEventListener("click", testimonialsModalFunc);
+
 
 // element toggle function
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
@@ -24,8 +62,6 @@ const filterFunc = function (selectedValue) {
   }
 
 }
-
-
 
 // contact form variables
 const form = document.querySelector("[data-form]");
@@ -54,9 +90,7 @@ const pages = document.querySelectorAll("[data-page]");
 let host;
 
 window.onload = function() {
-  console.log("hello");
   host = window.location.href.split('#')[0];
-  console.log(host)
 
   var lastIndex = window.location.href.lastIndexOf('#');
   if (lastIndex !== -1) {
