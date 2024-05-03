ed0f9f27f9a072560da9e9da1a13df4efa6ae736
mike911209 <mike911209@gmail.com>
2024-05-03 18:32:29 +0800

    blog: gallery

--- assets/js/lanternTraveling.js
+++ assets/js/lanternTraveling.js
@@ -44,6 +44,26 @@ overlay.addEventListener("click", testimonialsModalFunc);
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
 
+// custom select variables
+const select = document.querySelector("[data-select]");
+const selectItems = document.querySelectorAll("[data-select-item]");
+const selectValue = document.querySelector("[data-selecct-value]");
+const filterBtn = document.querySelectorAll("[data-filter-btn]");
+
+select.addEventListener("click", function () { elementToggleFunc(this); });
+
+// add event in all select items
+for (let i = 0; i < selectItems.length; i++) {
+  selectItems[i].addEventListener("click", function () {
+
+    let selectedValue = this.innerText.toLowerCase();
+    selectValue.innerText = this.innerText;
+    elementToggleFunc(select);
+    filterFunc(selectedValue);
+
+  });
+}
+
 // filter variables
 const filterItems = document.querySelectorAll("[data-filter-item]");
 
@@ -60,6 +80,25 @@ const filterFunc = function (selectedValue) {
     }
 
   }
+
+}
+
+// add event in all filter button items for large screen
+let lastClickedBtn = filterBtn[0];
+
+for (let i = 0; i < filterBtn.length; i++) {
+
+  filterBtn[i].addEventListener("click", function () {
+
+    let selectedValue = this.innerText.toLowerCase();
+    selectValue.innerText = this.innerText;
+    filterFunc(selectedValue);
+
+    lastClickedBtn.classList.remove("active");
+    this.classList.add("active");
+    lastClickedBtn = this;
+
+  });
 
 }
 
